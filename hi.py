63cc76718f86f2142b5347e97cae600729d12faf
AKHIL J R <akhiljr050@gmail.com>
2024-06-06 16:37:33 +0530

    commit

--- hi.py
+++ hi.py
@@ -1,1 +1,13 @@
-print("hai hello")
+class Solution(object):
+    def isPalindrome(self, x):
+        a=str(x)
+        b=a[::-1]
+        #b=b.rstrip("-")
+        c=int(b)
+        if x==c:
+            return True
+        else:
+            return False
+
+s=Solution()f
+print(s.isPalindrome(121))
